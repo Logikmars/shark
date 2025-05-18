73b43865374accb8421cc840e0feff65e05be7a9
ivandalechenko <ivandalechenko@gmail.com>
2025-05-18 04:07:29 +0300

    fix

--- src/components/Tokenomics/Tokenomics.jsx
+++ src/components/Tokenomics/Tokenomics.jsx
@@ -3,7 +3,15 @@ export default () => {
     return (
         <div className='Tokenomics container'>
             <div className='Tokenomics_title'>TOKENOMICS</div>
-
+            <div className='Tokenomics_content'>
+                {Array(10)
+                    .fill(0)
+                    .map((_, index) => {
+                        return <div className='Tokenomics_el free_img' key={`Tokenomics` + '_' + index}>
+                            <img src={`/img/tokenomics${index + 1}.svg`} alt="" />
+                        </div>
+                    })}
+            </div>
         </div>
     )
 }
